4d5f3c60453f82c4e9ed89cabc478f57b459b37b
Brian <bawilder@mtu.edu>
2025-04-03 18:42:59 -0400

    made CAN message class redundant by moving the ID to the UDS class to avoid repeated data and removed the CAN message class. added a custom Frame option. added ECU Reset protocols

--- src/uds-driver-can/uds_can.py
+++ src/uds-driver-can/uds_can.py
@@ -1,29 +1,5 @@
-class CANMessage:
-  """
-    Brief description of the class.
-
-    Attributes:
-        attribute1 (type): Description of attribute1.
-        attribute2 (type): Description of attribute2.
-  """
-  def __init__(self,id=0x0,data=[0x00,0x00,0x00,0x00]):
-    """
-    Initializes the ClassName with the given attributes.
-
-    Args:
-        attribute1 (type): Description of attribute1.
-        attribute2 (type): Description of attribute2.
-    """
-    self.id = id
-    self.data = data
-    self.length = len(data)
-  def convertToByteArray(self):
-    header = [self.id]
-    bytearray = header + self.data
-    return bytearray
-
 class UDSRequester:
-  """
+    """
     Brief description of the class.
 
     Attributes:
@@ -37,143 +13,188 @@ class UDSRequester:
       CodeClear CANMessage: Description of attribute2.
       PositiveCodeClearResponse CANMessage: Description of attribute2.
       NegativeCodeClearResponse CANMessage: Description of attribute2.
-  """
-  def __init__(self,tester,selfid=0x0,targetid=0x0):
     """
-    Initializes the ClassName with the given attributes.
 
-    Args:
-    
-    """
-    self.tester=tester
+    def __init__(self, tester, selfid=0x0, targetid=0x0):
+        """
+        Initializes the ClassName with the given attributes.
 
-    self.DefaultSession = CANMessage(targetid,[0x10,0x01])
-    self.ProgrammingSession = CANMessage(targetid,[0x10,0x02])
-    self.ExtendedSession = CANMessage(targetid,[0x10,0x03])
-    self.SafetySystemDiagnosticSession = CANMessage(targetid,[0x10,0x04])
-    self.PositiveSessionResponse = CANMessage(selfid,[0x50,0x03])
-    self.NegativeSessionResponse = CANMessage(selfid,[0x7F,0x10])
+        Args:
 
-    self.CodeClear = CANMessage(targetid,[0x14,0xFF,0xFF,0xFF])
-    self.PositiveCodeClearResponse = CANMessage(selfid,[0x54])
-    self.NegativeCodeClearResponse = CANMessage(selfid,[0x7F,0x14])
+        """
+        self.tester = tester
+        self.selfid = selfid
+        self.targetid = targetid
 
-    self.receivedResponse = ''
+        self.DefaultSession = [0x10, 0x01]
+        self.ProgrammingSession = [0x10, 0x02]
+        self.ExtendedSession = [0x10, 0x03]
+        self.SafetySystemDiagnosticSession = [0x10, 0x04]
+        self.PositiveSessionResponse = [0x50, 0x03]
+        self.NegativeSessionResponse = [0x7F, 0x10]
 
-  def receiveResponse(self):
-    resp = self.tester.recv()
-    self.receivedResponse = resp
-    return resp
+        self.CodeClear = [0x14]
+        self.PositiveCodeClearResponse = [0x54]
+        self.NegativeCodeClearResponse = [0x7F, 0x14]
 
-  def sendDefaultSession(self):
-    """
-    Displays the information of the example.
+        self.ECUReset_Hard = [0x11, 0x01]
+        self.ECUReset_KeyOffOn = [0x11, 0x02]
+        self.ECUReset_Soft = [0x11, 0x03]
+        self.PositiveECUResetResponse_Hard = [0x51, 0x01]
+        self.PositiveECUResetResponse_KeyOffOn = [0x51, 0x02]
+        self.PositiveECUResetResponse_Soft = [0x51, 0x03]
+        self.NegativeCodeClearResponse = [0x7F, 0x11]
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.tester.send(self.DefaultSession.convertToByteArray())
-    return
-  def sendProgrammingSession(self):
-    """
-    Displays the information of the example.
+        self.receivedResponse = ""
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.tester.send(self.ProgrammingSession.convertToByteArray())
-    return
-  def sendExtendedSession(self):
-    """
-    Displays the information of the example.
+    def updatetargetid(self, newTargetid):
+        self.targetid = newTargetid
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.tester.send(self.ExtendedSession.convertToByteArray())
-    return
-  def sendSafetySystemDiagnosticSession(self):
-    """
-    Displays the information of the example.
+    def receiveResponse(self):
+        resp = self.tester.recv()
+        self.receivedResponse = resp
+        return resp
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.tester.send(self.SafetySystemDiagnosticSession.convertToByteArray())
-    return
-  def checkPositiveSessionResponse(self,sessionType):
-    """
-    Displays the information of the example.
+    def sendCustomFrame(self, frameData):
+        data = list(bytearray.fromhex(frameData.replace(" ", "")))
+        self.tester.send(self.targetid, data)
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    resp = self.receiveResponse()
-    splitResp = resp.strip().lower().split(' ')
-    if (splitResp[1] == '50') and (sessionType == splitResp[2]):
-      return True
-    return False
-  def checkNegativeSessionResponse(self):
-    """
-    Displays the information of the example.
+    def sendSession(self, sessionType):
+        """
+        Displays the information of the example.
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    resp = self.receiveResponse()
-    splitResp = resp.strip().lower().split(' ')
-    if (splitResp[1] == '7f') and (splitResp[1] == '10'):
-      return True
-    return False
-  
-  def sendCodeClear(self):
-    """
-    Displays the information of the example.
+        Returns:
+            str: A string containing the name and value.
+        """
+        if sessionType == 1:
+            self.tester.send(self.targetid, self.DefaultSession)
+        elif sessionType == 2:
+            self.tester.send(self.targetid, self.ProgrammingSession)
+        elif sessionType == 3:
+            self.tester.send(self.targetid, self.ExtendedSession)
+        elif sessionType == 4:
+            self.tester.send(self.targetid, self.SafetySystemDiagnosticSession)
+        return
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.tester.send(self.CodeClear.convertToByteArray())
-    return
-  def checkPositiveCodeClearResponse(self):
-    """
-    Displays the information of the example.
+    def checkPositiveSessionResponse(self, sessionType):
+        """
+        Displays the information of the example.
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    resp = self.receiveResponse()
-    splitResp = resp.strip().lower().split(' ')
-    if splitResp[1] == '54':
-      return True
-    return False
-  def checkNegativeCodeClearResponse(self):
-    """
-    Displays the information of the example.
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if (splitResp[1] == "50") and (sessionType == splitResp[2]):
+            return True
+        return False
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    resp = self.receiveResponse()
-    splitResp = resp.strip().lower().split(' ')
-    if splitResp[1] == '7f' and (splitResp[1] == '14'):
-      return True
-    return False
+    def checkNegativeSessionResponse(self):
+        """
+        Displays the information of the example.
 
-  def CodeClearProtocol(self):
-    """
-    Displays the information of the example.
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if (splitResp[1] == "7f") and (splitResp[1] == "10"):
+            return True
+        return False
 
-    Returns:
-        str: A string containing the name and value.
-    """
-    self.sendExtendedSession()
-    if(self.checkPositiveSessionResponse('3') == False):
-      return self.receivedResponse
+    def sendCodeClear(self):
+        """
+        Displays the information of the example.
 
-    self.sendCodeClear()
-    return self.receiveResponse()
-    
-#ECU Reset
-#request VID
-  
+        Returns:
+            str: A string containing the name and value.
+        """
+        self.tester.send(self.targetid, self.CodeClear)
+        return
+
+    def checkPositiveCodeClearResponse(self):
+        """
+        Displays the information of the example.
+
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if splitResp[1] == "54":
+            return True
+        return False
+
+    def checkNegativeCodeClearResponse(self):
+        """
+        Displays the information of the example.
+
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if splitResp[1] == "7f" and (splitResp[1] == "14"):
+            return True
+        return False
+
+    def sendECUReset(self, resetType):
+        if resetType == 1:
+            self.tester.send(self.targetid, self.ECUReset_Hard)
+        elif resetType == 2:
+            self.tester.send(self.targetid, self.ECUReset_KeyOffOn)
+        elif resetType == 3:
+            self.tester.send(self.targetid, self.ECUReset_Soft)
+        return
+
+    def checkPositiveECUResetResponse(self, resetType):
+        """
+        Displays the information of the example.
+
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if (splitResp[1] == "51") and (resetType == splitResp[2]):
+            return True
+        return False
+
+    def checkNegativeECUResetResponse(self):
+        """
+        Displays the information of the example.
+
+        Returns:
+            str: A string containing the name and value.
+        """
+        resp = self.receiveResponse()
+        splitResp = resp.strip().lower().split(" ")
+        if splitResp[1] == "7f" and (splitResp[1] == "11"):
+            return True
+        return False
+
+    def CodeClearProtocol(self):
+        """
+        Displays the information of the example.
+
+        Returns:
+            str: A string containing the name and value.
+        """
+        self.sendSession(3)
+        if self.checkPositiveSessionResponse("3") == False:
+            return self.receivedResponse
+
+        self.sendCodeClear()
+        return self.receiveResponse()
+
+    def ECUResetProtocol(self, resetType):
+        self.sendSession(3)
+        if self.checkPositiveSessionResponse("3") == False:
+            return self.receivedResponse
+
+        self.sendECUReset(resetType)
+        return self.receiveResponse()
+
+
+# ECU Reset
+# request VID
